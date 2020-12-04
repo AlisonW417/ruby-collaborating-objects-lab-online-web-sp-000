a7ce4f3f73468dcea0c6d5f6e6009c35bac11652
Alison Williams <apw5011@gmail.com>
2020-12-04 21:56:14 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -19,9 +19,8 @@ class Song
   
   def self.new_by_filename(filename)
     artist_name, song_name, genre_name = filename.chomp(".mp3").split(" - ")
-    song = self.new(song_name)
-    song.artist = Artist.find_or_create_by_name(artist_name)
-    song.genre = genre_name
-    song.name 
+    song = Song.new(song_name)
+    song.artist_name = artist_name
+    song
   end 
 end 
