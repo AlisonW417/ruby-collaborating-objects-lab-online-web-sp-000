460f1518587aacde52fc469806531a9a345f1608
Alison Williams <apw5011@gmail.com>
2020-12-04 20:34:06 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -17,8 +17,16 @@ class Artist
     self.songs << new_song
   end 
   
-  def self.find_or_create_by_name(artist_name)
+  def self.find_or_create_by_name(name)
+    self.find_by_name(name) || self.create_by_name(name)
+  end 
   
+  def self.find_by_name(name)
+    @@all.detect {|artist| artist.name == name}
+  end 
+  
+  def self.create_by_name(name)
+    artist = self.new(name)
   end 
   
 end 
