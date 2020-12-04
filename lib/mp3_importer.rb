53a38de0e4326724f554c7f7773d35ee79f23f00
Alison Williams <apw5011@gmail.com>
2020-12-04 21:17:07 +0000

    Automatically backed up by Learn

--- lib/mp3_importer.rb
+++ lib/mp3_importer.rb
@@ -6,7 +6,9 @@ class MP3Importer
   end 
   
   def files 
-    Dir.glob("#{path}/*.mp3")
+    Dir.glob("#{path}/*.mp3").map do |file|
+      file.gsub("./spec/fixtures/mp3s/", "")
+    end 
   end 
 
   
