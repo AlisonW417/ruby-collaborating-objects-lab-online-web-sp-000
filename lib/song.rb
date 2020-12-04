3d8792fc396e629af5bd95bd4c0ab76fb0d528d6
Alison Williams <apw5011@gmail.com>
2020-12-04 21:45:23 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,5 +1,5 @@
 class Song 
-  attr_accessor :name, :artist 
+  attr_accessor :name, :artist, :genre
   
   @@all = []
   
@@ -18,6 +18,11 @@ class Song
   end 
   
   def self.new_by_filename(filename)
+    artist_name, song_name, genre_name = filename.chomp(".mp3").split(" - ")
+    song = self.new(song_name)
+    song.artist = Artist.find_or_create_by_name(artist_name)
+    song.genre = genre_name
+  end 
     
     
   
